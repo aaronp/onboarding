eda364bb0fea5693d93a356f0b9d998caab687b9
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-05-23 09:49:52 +0100

    created my counter component!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,16 @@
-<h1>Welcome to your library project</h1>
+<script>
+    let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+
+<h1>This is my onboarding componet! You clicked  {count} times!</h1>
+
+<button on:click={handleClick}>
+	clicks: {count}
+</button>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
