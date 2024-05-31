d5ac5541a7690cfc8701f783ff8a3a6d34d9787d
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-05-31 19:40:13 +0100

    created ops route with mermaid

---
+++ src/lib/Mermaid.svelte
@@ -0,0 +1,16 @@
+<!-- src/lib/Mermaid.svelte -->
+<script>
+    import { onMount } from 'svelte';
+    import mermaid from 'mermaid';
+  
+    export let chart;
+  
+    onMount(() => {
+      mermaid.initialize({ startOnLoad: true });
+      mermaid.contentLoaded();
+    });
+  </script>
+  
+  <div class="mermaid">
+    {@html chart}
+  </div>
---
+++ src/routes/ops/flow/+page.svelte
@@ -0,0 +1,26 @@
+<!-- src/routes/index.svelte -->
+<script>
+import Mermaid from '$lib/Mermaid.svelte';
+import { appBackend } from '$lib/stores/backend.js';
+import { Form, Field, TextField, SelectField, Button, Toggle, Dialog } from 'svelte-ux';
+let service;
+let page;
+let diagram;
+// Subscribe to the store
+appBackend.subscribe(value => {
+    service = value;
+    page = service.newFlowPage();
+    diagram = page.markdown();
+});
+
+    
+</script>
+  
+<Mermaid bind:chart={diagram} />
+  
+<style>
+ .mermaid {
+  text-align: center;
+  margin: 20px 0;
+}   
+</style>
